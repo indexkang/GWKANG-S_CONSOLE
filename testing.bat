7ca30b72838a61861f5ea4fd4495db76b158e915
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-02-24 14:06:31 +0530

    Update testing.bat

--- testing.bat
+++ testing.bat
@@ -4,5 +4,3 @@ if %1==Ripping_Tools curl -o installer.zip -s
 if %1==FF_Ripper curl -o installer.zip -s
 if %1==Unity curl -o installer.zip -s
 if %1==Blender curl -o installer.zip -s
-
-%3-pin=654321
