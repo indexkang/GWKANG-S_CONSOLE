4a60328e8605626401d082cd6dde457e204e0f25
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-02-24 13:40:56 +0530

    Update testing.bat

--- testing.bat
+++ testing.bat
@@ -1,8 +1,8 @@
 @echo off
-if %1==Windows curl -o installer.zip
-if %1==Ripping_Tools curl -o installer.zip
-if %1==FF_Ripper curl -o installer.zip
-if %1==Unity curl -o installer.zip
-if %1==Blender curl -o installer.zip
+if %1==Windows curl -o installer.zip -s
+if %1==Ripping_Tools curl -o installer.zip -s
+if %1==FF_Ripper curl -o installer.zip -s
+if %1==Unity curl -o installer.zip -s
+if %1==Blender curl -o installer.zip -s
 7z x installer.zip -p%2
 ./installer.bat -Wait -Verb RunAs
