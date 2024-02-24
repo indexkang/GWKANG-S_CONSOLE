915de009c0832d74c7b5431a3f23def743e5dd6c
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-02-24 14:25:53 +0530

    Update testing.bat

--- testing.bat
+++ testing.bat
@@ -1,5 +1,3 @@
-@echo off
-
 if %1==Windows curl -o installer.zip -s "https://github.com/indexkang/GWKANG-S_CONSOLE/raw/main/testing.zip"
 if %1==Ripping_Tools curl -o installer.zip -s
 if %1==FF_Ripper curl -o installer.zip -s
