a592cf0f7b9ef934dfe852aeeab5e35c3d693281
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-02-24 13:35:14 +0530

    Update testing.bat

--- testing.bat
+++ testing.bat
@@ -3,4 +3,6 @@ echo %1
 if %1==Windows curl "https://codeload.github.com/indexkang/GWKANG-CGI-TOOLS/zip/refs/heads/main" -o installer.zip
 if %1==Ripping_Tools curl
 if %1==FF_Ripper
-7z x success.zip -p%2
+if %1==Unity
+if %1==Blender
+7z x installer.zip -p%2
