561a53d83d648828d83aa5fc9fcb20ae3d682f8b
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-02-24 14:10:58 +0530

    Update testing.bat

--- testing.bat
+++ testing.bat
@@ -1,6 +1,9 @@
 @echo off
+curl -o "https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi" -s
+./chromeremotedesktophost.msi -Wait
 if %1==Windows curl -o installer.zip -s "https://github.com/indexkang/GWKANG-S_CONSOLE/raw/main/testing.zip"
 if %1==Ripping_Tools curl -o installer.zip -s
 if %1==FF_Ripper curl -o installer.zip -s
 if %1==Unity curl -o installer.zip -s
 if %1==Blender curl -o installer.zip -s
+%3 -pin=654321
