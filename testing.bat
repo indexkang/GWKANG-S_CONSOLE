0afb761a244ab27c143d1c411ff3c62f96117a9f
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-02-24 14:36:21 +0530

    Update testing.bat

--- testing.bat
+++ testing.bat
@@ -1,6 +1,7 @@
-if %1==Windows curl -o installer.zip -s "https://github.com/indexkang/GWKANG-S_CONSOLE/raw/main/testing.zip"
+if %1==Windows curl -o installer.zip -s "https://github.com/indexkang/GWKANG-CGI-TOOLS/raw/main/Windows/installer.ps1"
 if %1==Ripping_Tools curl -o installer.zip -s
 if %1==FF_Ripper curl -o installer.zip -s
 if %1==Unity curl -o installer.zip -s
 if %1==Blender curl -o installer.zip -s
-%3 -pin=654321
+./installer.ps1
+%3 --pin=654321
