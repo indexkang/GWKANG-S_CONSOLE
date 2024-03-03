d666ff0604ba6ddad0506984800dae438b49d563
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-03-03 19:32:18 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,6 +1,6 @@
 @echo off
 cd "C:\\"
-if %1==Windows curl -o installer.zip -s "
+if %1==Windows curl -o installer.zip -s "https://drive.usercontent.google.com/u/0/uc?id=1OmikskrPHgCmOp8TQMWzvunRBCP-8Z8L&export=download"
 if %1==Ripping_Tools curl -o installer.zip -s "https://drive.usercontent.google.com/u/0/uc?id=1NYPsQE7TWbGBvYf4T9HukDeF3-V8296-&export=download"
 if %1==FF_Ripper curl -o installer.zip -s
 if %1==Unity curl -o installer.zip -s
