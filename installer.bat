9b468b0b976567a5cb6ac23b694969ab3f0e642f
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-03-04 10:45:17 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,5 +1,6 @@
 @echo off
 cd "C:\\"
+if %1==Windows 
 curl -o files.zip -s "https://storage.googleapis.com/drive-bulk-export-anonymous/20240304T050737.037Z/4133399871716478688/bc77955a-47e9-4823-aa75-a65956bc9b73/1/8792e619-a103-4d83-ab88-cf4047628073?authuser"
 7z e files.zip -y
 7z x %1 -y -s
