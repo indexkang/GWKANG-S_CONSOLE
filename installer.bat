f02360df4fdec235d8b92975fce47a380c5b432a
Inderxkang <150659302+indexkang@users.noreply.github.com>
2024-03-04 11:28:48 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -4,7 +4,7 @@ if %1==FF_Ripper set TOOLNAME=ffripper.zip
 if %1==Blender set TOOLNAME=blender.zip
 if %1==Ripping_Tools set TOOLNAME=rippers.zip
 if %1==Unity set TOOLNAME=unity.zip
-curl -o files.zip -s "https://storage.googleapis.com/drive-bulk-export-anonymous/20240304T050737.037Z/4133399871716478688/bc77955a-47e9-4823-aa75-a65956bc9b73/1/8792e619-a103-4d83-ab88-cf4047628073?authuser"
+curl -o files.zip -s "https://storage.googleapis.com/drive-bulk-export-anonymous/20240304T055717.727Z/4133399871716478688/50557e20-a6b6-4d42-bab2-86ed88f88019/1/78699a4b-4539-4485-8430-74cd965b679d?authuser"
 7z e files.zip -y
 ls
 7z x %TOOLNAME% -p%2 -y 
